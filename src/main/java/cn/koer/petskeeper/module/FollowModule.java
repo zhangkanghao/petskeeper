0531649fc8d88aa520f3b15f3fb6a8499bb18540
koer <koerzhang@163.com>
2020-02-28 16:57:32 +0800

    FollowModule 查询粉丝和关注同时获取自己是否关注

--- src/main/java/cn/koer/petskeeper/module/FollowModule.java
+++ src/main/java/cn/koer/petskeeper/module/FollowModule.java
@@ -5,15 +5,26 @@ import cn.koer.petskeeper.bean.UserProfile;
 import org.nutz.dao.Cnd;
 import org.nutz.dao.Condition;
 import org.nutz.dao.QueryResult;
+import org.nutz.dao.Sqls;
+import org.nutz.dao.entity.Entity;
+import org.nutz.dao.entity.Record;
 import org.nutz.dao.pager.Pager;
+import org.nutz.dao.sql.Sql;
+import org.nutz.dao.sql.SqlCallback;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.lang.util.NutMap;
 import org.nutz.mvc.Scope;
-import org.nutz.mvc.annotation.At;
-import org.nutz.mvc.annotation.Attr;
-import org.nutz.mvc.annotation.Param;
+import org.nutz.mvc.annotation.*;
+import org.nutz.mvc.filter.CheckSession;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.alibaba.fastjson.JSONObject;
 
 
 /**
@@ -22,25 +33,57 @@ import java.util.Date;
  */
 @At("/follow")
 @IocBean
+@Filters(@By(type = CheckSession.class, args = {"ident", "/"}))
 public class FollowModule extends BaseModule {
 
+    private SqlCallback getCallback(){
+        return new SqlCallback() {
+            @Override
+            public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
+                List<JSONObject> list= new ArrayList<>();
+                while(rs.next()){
+                    JSONObject obj=new JSONObject();
+                    obj.put("userId",rs.getInt("uid"));
+                    obj.put("nickname",rs.getString("nickname"));
+                    obj.put("avatar",rs.getBlob("avatar"));
+                    obj.put("description",rs.getString("dt"));
+                    obj.put("praise",rs.getInt("praise"));
+                    obj.put("follower",rs.getInt("follower"));
+                    obj.put("id",rs.getInt("id"));
+                    list.add(obj);
+                }
+                return list;
+            }
+        };
+    }
+
     @At("/follower")
-    public Object getFollower(@Param("userId") int userId) {
-        /**select * from t_user_profile where uid in(select from_id from t_follow where to_id =%d)*/
-        QueryResult rs = new QueryResult();
-        Condition cnd = Cnd.wrap("uid in (select from_id from t_follow where to_id =${userId}) ", new NutMap().setv("userId",userId));
-        rs.setList(dao.query(UserProfile.class, cnd, null));
-        rs.setPager(null);
-        return rs;
+    public Object getFollower(@Param("userId") int userId,@Attr(scope = Scope.SESSION,value = "ident")int me) {
+        Sql sql= Sqls.create("select uid,nickname,avatar,dt,praise,follower,id from t_user_profile u left join  (select * from t_follow where from_id=@ident_id) b on u.uid=b.to_id " +
+                "where uid in (select from_id from t_follow where to_id =@to_id)");
+        sql.setParam("ident_id",me);
+        sql.setParam("to_id",userId);
+        sql.setCallback(getCallback());
+        Entity<Record> entity = dao.getEntity(Record.class);
+        sql.setEntity(entity);
+        dao.execute(sql);
+        List<Record> users = sql.getList(Record.class);
+        return new NutMap().setv("ok",true).setv("data",users);
+
     }
 
     @At("/following")
-    public Object getFollowing(@Param("userId") int userId) {
-        QueryResult rs = new QueryResult();
-        Condition cnd = Cnd.wrap("uid in(select to_id from t_follow where from_id=${userId})", new NutMap().setv("userId",userId));
-        rs.setList(dao.query(UserProfile.class, cnd, null));
-        rs.setPager(null);
-        return rs;
+    public Object getFollowing(@Param("userId") int userId,@Attr(scope = Scope.SESSION,value = "ident")int me) {
+        Sql sql= Sqls.create("select uid,nickname,avatar,dt,praise,follower,id from t_user_profile u left join  (select * from t_follow where from_id=@ident_id) b on u.uid=b.to_id " +
+                "where uid in (select to_id from t_follow where from_id =@from_id)");
+        sql.setParam("ident_id",me);
+        sql.setParam("from_id",userId);
+        sql.setCallback(getCallback());
+        Entity<Record> entity = dao.getEntity(Record.class);
+        sql.setEntity(entity);
+        dao.execute(sql);
+        List<Record> users = sql.getList(Record.class);
+        return new NutMap().setv("ok",true).setv("data",users);
     }
 
     @At
---
+++ src/main/java/cn/koer/petskeeper/service/FollowService.java
@@ -0,0 +1,13 @@
+package cn.koer.petskeeper.service;
+
+import cn.koer.petskeeper.bean.Follow;
+import org.nutz.ioc.loader.annotation.IocBean;
+
+/**
+ * @Author Koer
+ * @Date 2020/2/28 13:49
+ */
+@IocBean(fields = "dao")
+public class FollowService {
+
+}
