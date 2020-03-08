1b9f5cb1d06caa9b5b750cfa39811883bb7e7070
koer <koerzhang@163.com>
2020-03-08 10:02:14 +0800

    动作链删除shiro shiro依赖删除

--- src/main/resources/mvc/petskeeper-mvc-chain.js
+++ src/main/resources/mvc/petskeeper-mvc-chain.js
@@ -5,7 +5,6 @@ var chain={
             "org.nutz.mvc.impl.processor.UpdateRequestAttributesProcessor",
             "org.nutz.mvc.impl.processor.EncodingProcessor",
             "org.nutz.mvc.impl.processor.ModuleProcessor",
-            "!org.nutz.integration.shiro.NutShiroProcessor",
             "org.nutz.mvc.impl.processor.ActionFiltersProcessor",
             "org.nutz.mvc.impl.processor.AdaptorProcessor",
             "org.nutz.mvc.impl.processor.MethodInvokeProcessor",
