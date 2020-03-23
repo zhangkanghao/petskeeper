3ea79cd1177331776898358efd6e4fad5a39e06d
koer <koerzhang@163.com>
2020-03-23 11:54:08 +0800

    交互：文章点赞

--- src/main/java/cn/koer/petskeeper/module/PraiseModule.java
+++ src/main/java/cn/koer/petskeeper/module/PraiseModule.java
@@ -4,16 +4,16 @@ import cn.koer.petskeeper.bean.Article;
 import cn.koer.petskeeper.bean.Comment;
 import cn.koer.petskeeper.bean.Pet;
 import cn.koer.petskeeper.bean.Praise;
+import cn.koer.petskeeper.filter.CheckTokenFilter;
 import org.nutz.dao.Chain;
 import org.nutz.dao.Cnd;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.lang.util.NutMap;
-import org.nutz.mvc.Scope;
 import org.nutz.mvc.annotation.*;
-import org.nutz.mvc.filter.CheckSession;
 import org.nutz.trans.Atom;
 import org.nutz.trans.Trans;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 /**
@@ -21,30 +21,48 @@ import java.util.Date;
  * @Date 2020/2/29 16:14
  */
 @IocBean
-@Filters(@By(type = CheckSession.class, args = {"ident", "/"}))
+@Filters(@By(type = CheckTokenFilter.class))
 @At("/praise")
 public class PraiseModule extends BaseModule {
 
     @At
-    public Object add(@Param("..") final Praise praise, @Attr(scope = Scope.SESSION,value = "ident")int me){
-        praise.setUserId(me);
+    public Object get(@Param("type")int type, @Param("targetId") final int targetId, HttpServletRequest req){
+        int userId= (int) req.getAttribute("uid");
+        Praise praise=dao.fetch(Praise.class,Cnd.where("type","=",type).and("userId","=",userId).and("targetId","=",targetId));
+        if(praise==null){
+            return new NutMap("ok",false);
+        }
+        return new NutMap().setv("ok",true);
+    }
+
+    @At
+    public Object add(@Param("type")int type, @Param("targetId") final int targetId, HttpServletRequest req){
+        int userId= (int) req.getAttribute("uid");
+        Praise praise1=dao.fetch(Praise.class,Cnd.where("type","=",type).and("userId","=",userId).and("targetId","=",targetId));
+        if(praise1!=null){
+            return new NutMap().setv("ok",false).setv("msg","已经点过赞了");
+        }
+        final Praise praise=new Praise();
+        praise.setUserId(userId);
+        praise.setType(type);
+        praise.setTargetId(targetId);
         praise.setCreateTime(new Date());
         praise.setUpdateTime(new Date());
-        if(praise.getType()==0){
+        if(type==0){
             //给文章点赞
             Trans.exec(new Atom() {
                 @Override
                 public void run() {
-                    dao.update(Article.class, Chain.makeSpecial("praise","+1"), Cnd.where("id","=",praise.getTargetId()));
+                    dao.update(Article.class, Chain.makeSpecial("praise","+1"), Cnd.where("id","=",targetId));
                     dao.insert(praise);
                 }
             });
-        } else if (praise.getType() == 1) {
+        } else if (type == 1) {
             //给评论点赞
             Trans.exec(new Atom() {
                 @Override
                 public void run() {
-                    dao.update(Comment.class,Chain.makeSpecial("praise","+1"),Cnd.where("id","=",praise.getTargetId()));
+                    dao.update(Comment.class,Chain.makeSpecial("praise","+1"),Cnd.where("id","=",targetId));
                     dao.insert(praise);
                 }
             });
@@ -53,7 +71,7 @@ public class PraiseModule extends BaseModule {
             Trans.exec(new Atom() {
                 @Override
                 public void run() {
-                    dao.update(Pet.class,Chain.makeSpecial("praise","+1"),Cnd.where("id","=",praise.getTargetId()));
+                    dao.update(Pet.class,Chain.makeSpecial("praise","+1"),Cnd.where("id","=",targetId));
                     dao.insert(praise);
                 }
             });
@@ -64,22 +82,27 @@ public class PraiseModule extends BaseModule {
     }
 
     @At
-    public Object remove(@Param("..") final Praise praise, @Attr(scope = Scope.SESSION,value = "ident")int me){
-        if(praise.getType()==0){
+    public Object remove(@Param("type")int type, @Param("targetId") final int targetId, HttpServletRequest req){
+        int userId= (int) req.getAttribute("uid");
+        final Praise praise=dao.fetch(Praise.class,Cnd.where("type","=",type).and("userId","=",userId).and("targetId","=",targetId));
+        if (praise==null){
+            return new NutMap().setv("ok",false).setv("msg","还没有点赞呢");
+        }
+        if(type==0){
             //给文章点赞
             Trans.exec(new Atom() {
                 @Override
                 public void run() {
-                    dao.update(Article.class, Chain.makeSpecial("praise","-1"), Cnd.where("id","=",praise.getTargetId()));
+                    dao.update(Article.class, Chain.makeSpecial("praise","-1"), Cnd.where("id","=",targetId));
                     dao.delete(praise);
                 }
             });
-        } else if (praise.getType() == 1) {
+        } else if (type == 1) {
             //给评论点赞
             Trans.exec(new Atom() {
                 @Override
                 public void run() {
-                    dao.update(Comment.class,Chain.makeSpecial("praise","-1"),Cnd.where("id","=",praise.getTargetId()));
+                    dao.update(Comment.class,Chain.makeSpecial("praise","-1"),Cnd.where("id","=",targetId));
                     dao.delete(praise);
                 }
             });
@@ -88,7 +111,7 @@ public class PraiseModule extends BaseModule {
             Trans.exec(new Atom() {
                 @Override
                 public void run() {
-                    dao.update(Pet.class,Chain.makeSpecial("praise","-1"),Cnd.where("id","=",praise.getTargetId()));
+                    dao.update(Pet.class,Chain.makeSpecial("praise","-1"),Cnd.where("id","=",targetId));
                     dao.delete(praise);
                 }
             });
